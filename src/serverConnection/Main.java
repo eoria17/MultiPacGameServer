b5eacaf8ef14a00b36361a79fa92b8d08bb4ae38
Theo <theoriandy.17@gmail.com>
2020-09-08 20:51:19 +1000

    fix

--- src/serverConnection/Main.java
+++ src/serverConnection/Main.java
@@ -4,7 +4,7 @@ public class Main {
 
 	public static void main(String[] args) {
 		
-		GameServer gameServer = new GameServer(8080);
+		serverConnection.GameServer gameServer = new GameServer(2000);
 		gameServer.start();
 	}
 
